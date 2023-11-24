3153e1a344d725be68c716716d6b57943ba9236a
D4Enjoyer <146950055+D4Enjoyer@users.noreply.github.com>
2023-11-24 20:01:11 +0100

    Update poe-trade-fuzzy-search.user.js

--- poe-trade-fuzzy-search.user.js
+++ poe-trade-fuzzy-search.user.js
@@ -11,7 +11,7 @@
 let fuzzySearchActivated = true;
 
 document.body.addEventListener("keydown", handleFuzzySearch);
-document.body.addEventListener("paste", function(e) { setTimeout(handleFuzzySearch, 0, e) });
+document.body.addEventListener("paste", function(e) {setTimeout(handleFuzzySearch, 0, e)});
 
 // Function to handle the main fuzzy search logic
 function handleFuzzySearch(e) {
